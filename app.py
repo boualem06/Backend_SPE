ae27bbe7a0231f6adf2e0be66b9e541441275c9e
RayaneA7 <ka_kebir@esi.dz>
2023-08-28 23:03:58 +0100

    added some fixs

--- app.py
+++ app.py
@@ -15,6 +15,17 @@ app = Flask(__name__)
 
 CORS(app)
 
+
+@app.route('/vars')
+def get_vars():
+    file_path = './data2.csv'
+    data = pd.read_csv(file_path, delimiter=';', encoding='ISO-8859-1')
+    data = data.drop(data.iloc[:, 0 :13], axis = 1)
+    data.rename({'annï¿½ï¿½':'AnneeDeNaissance'},axis=1,  inplace=True, errors='raise')
+    print(data.columns.tolist())
+
+    return jsonify(data.columns.tolist())
+
 @app.route('/PCA')
 def PcaImplementation():
     file_path = './data2.csv'
@@ -31,6 +42,7 @@ def PcaImplementation():
 
 
 # Handle missing values if needed
+
     numeric_data.fillna(0, inplace=True)
 
 # Performing PCA
