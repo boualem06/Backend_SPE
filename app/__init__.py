9d07a0a9fc7e5ec2e585f5614ea613f2a92d7aa8
boualem06 <kb_hamroune@esi.dz>
2023-08-28 23:37:00 +0100

    solving the problem of cors

--- app/__init__.py
+++ app/__init__.py
@@ -12,13 +12,14 @@
 
 
 from flask import Flask
-
+from flask_cors import CORS
 # Import the register_routes function from app/routes/__init__.py
 from .routes import register_routes
 
 def create_app():
     app = Flask(__name__)
 
+    CORS(app)
     # Call the register_routes function to register the routes
     register_routes(app)
 
