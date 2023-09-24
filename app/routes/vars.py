9d3a53047a4b0b1b71aff0b15bb2425af694419d
RayaneA7 <ka_kebir@esi.dz>
2023-09-24 07:14:10 +0100

    added vars endpoint to retreive vars

--- app/routes/vars.py
+++ app/routes/vars.py
@@ -11,7 +11,7 @@ vars_bp = Blueprint('vars', __name__)
 
 @vars_bp.route('/vars')
 def get_vars():
-    file_path = './data2.csv'
+    file_path = request.args.get('file')
     data = pd.read_csv(file_path, delimiter=';', encoding='ISO-8859-1')
     data = data.drop(data.iloc[:, 0:13], axis=1)
     # data.rename({'annï¿½ï¿½': 'AnneeDeNaissance'},
