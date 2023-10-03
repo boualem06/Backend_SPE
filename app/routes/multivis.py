ab937c784baf7f79101d7acc0318bb5b7412fcf2
RayaneA7 <ka_kebir@esi.dz>
2023-10-03 22:46:03 +0100

    fixed folder structure

--- app/routes/multivis.py
+++ app/routes/multivis.py
@@ -1,15 +1,16 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-from flask import Flask, request, jsonify,Blueprint
+from flask import Flask, request, jsonify, Blueprint
 from fuzzywuzzy import fuzz
 from fuzzywuzzy import process
 
 
 from data_utils import load_data
 
-multivis_bp = Blueprint('multivis', __name__)
+multivis_bp = Blueprint("multivis", __name__)
 
-var="hello"
+var = "hello"
+
 
 def levenshtein_distance(word1, word2):
     if len(word1) < len(word2):
@@ -31,13 +32,19 @@ def levenshtein_distance(word1, word2):
     return previous_row[-1]
 
 
-def highest_sim_word(word_list): #these function allows us to calculate the word that had the heighest similarities to the other words 
-    
-    most_similar_word = min(word_list, key=lambda word: sum(levenshtein_distance(word, other) for other in word_list))
+def highest_sim_word(
+    word_list,
+):  # these function allows us to calculate the word that had the heighest similarities to the other words
+    most_similar_word = min(
+        word_list,
+        key=lambda word: sum(levenshtein_distance(word, other) for other in word_list),
+    )
     return most_similar_word
 
-def calculating_similarities(data): #these function allows us to calculate the similaire words 
-    
+
+def calculating_similarities(
+    data,
+):  # these function allows us to calculate the similaire words
     threshold = 70  # Adjust the threshold as needed
 
     similar_words_data = []
@@ -49,83 +56,102 @@ def calculating_similarities(data): #these function allows us to calculate the s
 
         for word in response_counts.keys():
             matches = process.extract(word, response_counts.keys(), scorer=fuzz.ratio)
-            similar_words[word] = [match for match, score in matches if score >= threshold and match != word]
+            similar_words[word] = [
+                match
+                for match, score in matches
+                if score >= threshold and match != word
+            ]
 
-        similar_words_data.append({"column_name":column_name,
-                                  "response_counts":similar_words})
+        similar_words_data.append(
+            {"column_name": column_name, "response_counts": similar_words}
+        )
 
-    return (similar_words_data)
+    return similar_words_data
 
 
 # ********************************************************************************
 
-def sum_similar_words(plt_count,arra_sim):  #these function allows sum the values of similaire words together and take only the word theat have the max of similarities in these words
-    final_res=[]
-    arr_word_sim=[]
-    temp_array=[]
+
+def sum_similar_words(
+    plt_count, arra_sim
+):  # these function allows sum the values of similaire words together and take only the word theat have the max of similarities in these words
+    final_res = []
+    arr_word_sim = []
+    temp_array = []
     for index, elem in enumerate(plt_count):
-        arr_word_sim=[]
-        column_name=elem["column_name"]
-        response_counts=elem["response_counts"]
-    
-        search_key = 'column_name'
+        arr_word_sim = []
+        column_name = elem["column_name"]
+        response_counts = elem["response_counts"]
+
+        search_key = "column_name"
         search_value = column_name
-        elem_arra_sim=None
-    
+        elem_arra_sim = None
+
         for i, d in enumerate(arra_sim):
             if search_key in d and d[search_key] == search_value:
-                elem_arra_sim=d
+                elem_arra_sim = d
                 break
-        final_dic={}
-        temp_dict={}
-        final_dic["column_name"]=column_name 
-        final_dic["values"]=[]
-        temp_dict["column_name"]=column_name 
-       
+        final_dic = {}
+        temp_dict = {}
+        final_dic["column_name"] = column_name
+        final_dic["values"] = []
+        temp_dict["column_name"] = column_name
+
         for word, count in response_counts.items():
-            list_similaire_words=elem_arra_sim["response_counts"][word]
+            list_similaire_words = elem_arra_sim["response_counts"][word]
             list_similaire_words.append(word)
-            list_similaire_words=[w for w in list_similaire_words if w not in arr_word_sim]
-        
-            count=0
+            list_similaire_words = [
+                w for w in list_similaire_words if w not in arr_word_sim
+            ]
+
+            count = 0
             for word_sim in list_similaire_words:
-                if word_sim in response_counts :
-                    count=count+response_counts[word_sim]
+                if word_sim in response_counts:
+                    count = count + response_counts[word_sim]
                     arr_word_sim.append(word_sim)
-            if(list_similaire_words)    :
-                word_heighest_sim=highest_sim_word(list_similaire_words)
-              
-                final_dic["values"].append({column_name:word_heighest_sim,"value":count})
-                temp_dict[word_heighest_sim]=list_similaire_words
-       
-        
-        
+            if list_similaire_words:
+                word_heighest_sim = highest_sim_word(list_similaire_words)
+
+                final_dic["values"].append(
+                    {column_name: word_heighest_sim, "value": count}
+                )
+                temp_dict[word_heighest_sim] = list_similaire_words
+
         final_res.append(final_dic)
         temp_array.append(temp_dict)
-    return (final_res,temp_array)
+    return (final_res, temp_array)
 
-@multivis_bp.route('/multivis', methods=['POST'])
+
+@multivis_bp.route("/multivis", methods=["POST"])
 def visualize_data_and_plot():
     try:
-        file_path = "data2.csv"  # Replace with your actual file path
-        df = pd.read_csv(file_path, delimiter=';', encoding='ISO-8859-1')
+        file_path = "./files/data2.csv"  # Replace with your actual file path
+        df = pd.read_csv(file_path, delimiter=";", encoding="ISO-8859-1")
 
-        column_names = request.json['column_names']
+        column_names = request.json["column_names"]
 
         all_response_data = []
 
         for column_name in column_names:
             # Get the correct column name using the df.columns attribute
             desired_column_name = column_name
-            actual_column_name = [col for col in df.columns if desired_column_name.lower() in col.lower()][0]
+            actual_column_name = [
+                col for col in df.columns if desired_column_name.lower() in col.lower()
+            ][0]
 
             # Extract unique responses from the desired column
             all_responses = []
 
             # Iterate through each entry and split multiple responses
             for entry in df[actual_column_name]:
-                responses = entry.split(',')
-                all_responses.extend([response.lower().strip() for response in responses if response.strip() != ""])
+                responses = entry.split(",")
+                all_responses.extend(
+                    [
+                        response.lower().strip()
+                        for response in responses
+                        if response.strip() != ""
+                    ]
+                )
 
             # Convert the list of responses into a Series
             response_series = pd.Series(all_responses)
@@ -138,15 +164,13 @@ def visualize_data_and_plot():
 
             response_data = {
                 "column_name": column_name,
-                "response_counts": response_counts_dict
+                "response_counts": response_counts_dict,
             }
             all_response_data.append(response_data)
 
-        arra_sim=calculating_similarities(all_response_data)
-        result,temp_array = sum_similar_words(all_response_data, arra_sim)
+        arra_sim = calculating_similarities(all_response_data)
+        result, temp_array = sum_similar_words(all_response_data, arra_sim)
         return jsonify(result)
 
     except Exception as e:
         return jsonify({"error": str(e)})
-
-
--- app/routes/vars.py
+++ app/routes/vars.py
@@ -1,5 +1,3 @@
-
-
 from flask import Flask, jsonify, request, send_file
 from flask import Blueprint
 import pandas as pd
@@ -7,12 +5,13 @@ from flask import jsonify
 import os
 import io
 
-vars_bp = Blueprint('vars', __name__)
+vars_bp = Blueprint("vars", __name__)
 
-@vars_bp.route('/vars')
+
+@vars_bp.route("/vars")
 def get_vars():
-    file_path = request.args.get('file')
-    data = pd.read_csv(file_path, delimiter=';', encoding='ISO-8859-1')
+    file_path = request.args.get("file")
+    data = pd.read_csv(file_path, delimiter=";", encoding="ISO-8859-1")
     data = data.drop(data.iloc[:, 0:13], axis=1)
     # data.rename({'annï¿½ï¿½': 'AnneeDeNaissance'},
     #             axis=1,  inplace=True, errors='raise')
@@ -34,65 +33,57 @@ def get_vars():
     # print(csv_data)
     response_data = []
     for res in data.columns.tolist():
-        response_data.append({
-            "key":res, "value":res
-        })
+        response_data.append({"key": res, "value": res})
 
     return jsonify(response_data)
 
 
-@vars_bp.route('/download', methods=['GET'])
+@vars_bp.route("/download", methods=["GET"])
 def download():
     # Get parameters from the query string
-    data_type = request.args.get('data_type')
-    original_filename = request.args.get('original_filename')
+    data_type = request.args.get("data_type")
+    original_filename = request.args.get("original_filename")
 
     # Check if parameters are valid
-    if not (data_type in ['json', 'csv'] and original_filename):
-        return 'Invalid parameters', 400
+    if not (data_type in ["json", "csv"] and original_filename):
+        return "Invalid parameters", 400
 
-    data = pd.read_csv(original_filename, delimiter=';', encoding='ISO-8859-1')
+    data = pd.read_csv(original_filename, delimiter=";", encoding="ISO-8859-1")
     data = data.drop(data.iloc[:, 0:13], axis=1)
 
-    if data_type == 'json':
-        data_str = pd.DataFrame(data).to_json(orient='records')
-        download_name = 'data.json'
-        mimetype = 'application/json'
-    elif data_type == 'csv':
+    if data_type == "json":
+        data_str = pd.DataFrame(data).to_json(orient="records")
+        download_name = "data.json"
+        mimetype = "application/json"
+    elif data_type == "csv":
         data_str = pd.DataFrame(data).to_csv(index=False)
-        download_name = 'data.csv'
-        mimetype = 'text/csv'
-    data_stream = io.BytesIO(data_str.encode('utf-8'))
+        download_name = "data.csv"
+        mimetype = "text/csv"
+    data_stream = io.BytesIO(data_str.encode("utf-8"))
 
     response = send_file(
-        data_stream,
-        as_attachment=True,
-        download_name=download_name,
-        mimetype=mimetype
+        data_stream, as_attachment=True, download_name=download_name, mimetype=mimetype
     )
 
     return response
 
 
-@vars_bp.route('/filesList', methods=['GET'])
+@vars_bp.route("/filesList", methods=["GET"])
 def filesList():
     root_folder = os.getcwd()  # Get the current working directory as the root folder
-    csv_files = [f for f in os.listdir(root_folder) if f.endswith('.csv')]
+    csv_files = [f for f in os.listdir(root_folder + "/files") if f.endswith(".csv")]
     csv_data = []
 
     for filename in os.listdir(root_folder):
-        if filename.endswith('.csv'):
+        if filename.endswith(".csv"):
             data_info = {}
             file_path = os.path.join(root_folder, filename)
-            data = pd.read_csv(file_path, delimiter=';', encoding='ISO-8859-1')
-            # data = data.drop(data.iloc[:, 13:], axis=1)        
-            data_info['titre'] = data.loc[0,'titre']
-            data_info['date'] = data.loc[0,'datecreat']
-            data_info['user'] = data.loc[0,'username']
+            data = pd.read_csv(file_path, delimiter=";", encoding="ISO-8859-1")
+            # data = data.drop(data.iloc[:, 13:], axis=1)
+            data_info["titre"] = data.loc[0, "titre"]
+            data_info["date"] = data.loc[0, "datecreat"]
+            data_info["user"] = data.loc[0, "username"]
 
-            csv_data.append({
-                'filename': filename,
-                'data': data_info
-            })
+            csv_data.append({"filename": filename, "data": data_info})
 
     return jsonify(csv_data)
